8e9b42334b5640c223fdf35edd6e2d6f1a9f4d24
Ashish Mishra <ashishk861@gmail.com>
2020-08-12 21:40:08 -0400

    Update BinaryTreeApp.java

--- src/BinarySearchTree/BinaryTreeApp.java
+++ src/BinarySearchTree/BinaryTreeApp.java
@@ -5,7 +5,7 @@ public class BinaryTreeApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		BinaryTree bst=new BinaryTree();
+		BinaryTree bst = new BinaryTree();
 //		Node root=bst.createNode(2);
 //		root.left=bst.createNode(7);
 //		root.right=bst.createNode(5);
@@ -15,28 +15,38 @@ public class BinaryTreeApp {
 //		root.left.right.right=bst.createNode(11);
 //		root.right.right=bst.createNode(9);
 //		root.right.right.left=bst.createNode(4);
-		
-		BinaryTree a=new BinaryTree();
-		Node root = a.createNode(8);
-	    root.left = a.createNode(3);
-	    root.right = a.createNode(10);
-	    root.left.left = a.createNode(1);
-	    root.left.right = a.createNode(6);
-	    root.left.right.left = a.createNode(4);
-	    root.left.right.right = a.createNode(7);
-	    root.right.right = a.createNode(14);
-	    root.right.right.left = a.createNode(13);
-	    
-	    Node root1 = a.createNode(8);
-	    root1.left = a.createNode(3);
-	    root1.right = a.createNode(10);
-	    root1.left.left = a.createNode(1);
-	    root1.left.right = a.createNode(6);
-	    root1.left.right.left = a.createNode(4);
-	    root1.left.right.right = a.createNode(7);
-	    root1.right.right = a.createNode(14);
-	    root1.right.right.left = a.createNode(18);
-		
+
+		Node root = bst.createNode(2);
+		root.left = bst.createNode(7);
+		root.right = bst.createNode(5);
+		root.left.left = bst.createNode(2);
+		root.left.right = bst.createNode(6);
+		root.left.right.left = bst.createNode(5);
+		root.left.right.right = bst.createNode(11);
+		root.right.right = bst.createNode(9);
+		root.right.right.left = bst.createNode(4);
+
+		BinaryTree a = new BinaryTree();
+//		Node root = a.createNode(8);
+//	    root.left = a.createNode(3);
+//	    root.right = a.createNode(10);
+//	    root.left.left = a.createNode(1);
+//	    root.left.right = a.createNode(6);
+//	    root.left.right.left = a.createNode(4);
+//	    root.left.right.right = a.createNode(7);
+//	    root.right.right = a.createNode(14);
+//	    root.right.right.left = a.createNode(13);
+
+//		Node root1 = a.createNode(8);
+//		root1.left = a.createNode(3);
+//		root1.right = a.createNode(10);
+//		root1.left.left = a.createNode(1);
+//		root1.left.right = a.createNode(6);
+//		root1.left.right.left = a.createNode(4);
+//		root1.left.right.right = a.createNode(7);
+//		root1.right.right = a.createNode(14);
+//		root1.right.right.left = a.createNode(18);
+
 //		System.out.println("The inorder traversal is");
 //		bst.inOrder(root);
 //		System.out.println();
@@ -66,13 +76,13 @@ public class BinaryTreeApp {
 //		
 //		System.out.println("The level order print is :");
 //		bst.levelOrderPrint(root);
-		
+
 //		System.out.println("The level order print is :");
 //		bst.levelOrderQueue(root);
-		
+
 //		System.out.println("The reverse level order print is :");
 //		bst.reverLevelOrderQueue(root);
-		
+
 //		System.out.println("The step by step level order :");
 //		bst.levelStepQueue(root);
 //		
@@ -81,16 +91,16 @@ public class BinaryTreeApp {
 //		
 //		System.out.println("Only left elements :");
 //		bst.leftViewTree(root,0);
-		
+
 //		System.out.println("Only right elements :");
 //		bst.rightViewTree(root,0);
-		
+
 //		System.out.println("Inorder without recursion :");
 //		bst.inordNoRec(root);
-		
+
 //		System.out.println("Preorder without recursion :");
 //		bst.preOrdNoRec(root);
-		
+
 //		System.out.println("PostOrder without recursion :");
 //		bst.postOrderWitRecur(root);
 //		System.out.println();
@@ -99,17 +109,86 @@ public class BinaryTreeApp {
 //		Node mirror=bst.mirroBinaryTree(root);
 //		bst.inOrder(mirror);
 //		System.out.println();
-		
+
 //		System.out.println("Deleting the full tree :");
 //		bst.deleteTree(root);
 //		System.out.println();
-		
+
 //		System.out.println("Check if two nodes are identical :");
 //		System.out.println(a.checkIfIdentical(root,root1));
 //		System.out.println();
+
+//		System.out.println("Leve of a binar tree :");
+//		System.out.println(a.getLevelOfNode(root, 7, 1));
+//		System.out.println();
+//		
+//		NodeHeight root2=a.createNewNode(8);
+//		root2.left = a.createNewNode(3);
+//		root2.right = a.createNewNode(10);
+//		root2.left.left = a.createNewNode(1);
+//		root2.left.right = a.createNewNode(6);
+//		root2.left.right.left = a.createNewNode(4);
+//		root2.left.right.right = a.createNewNode(7);
+//		root2.right.right = a.createNewNode(14);
+//		root2.right.right.left = a.createNewNode(18);
+//		
+//		System.out.println("Top view of a binary tree :");
+//		a.prinTopView(root2);
+//		System.out.println();
+
+//		System.out.println("The left boundary print is :");
+//		bst.printLeftBoundary(root);
+//		System.out.println();
+//		
+//		System.out.println("The right boundary print is :");
+//		bst.printRightBoundary(root);
+//		System.out.println();
+//		
+//		System.out.println("The leave node print is :");
+//		bst.printLeaveNodes(root);
+//		System.out.println();
+
+//		System.out.println("The vertical level order is :");
+//		bst.printVerticalOrder(root);
+//		System.out.println();
+//		
+//		System.out.println("The vertical level order sum is :");
+//		bst.sumVerticalOrder(root);
+//		System.out.println();
+
+//		System.out.println("The level by level sum is :");
+//		bst.sumEachLevel(root);
+//		System.out.println();
+//		
+//		System.out.println("Searching an element in BST :");
+//		System.out.println(bst.serachBinaryTree(root,2));
+//		System.out.println();
+
+//		System.out.println("Printing a tree in spiral order :");
+//		bst.printSpiralForm(root);
+//		System.out.println();
+
+//		System.out.println("Printing the elememnts between any two levels :");
+//		bst.printBetweenLevels(root, 1, 3);
+//		System.out.println();
+
+		// If both the trees are mirrors
+		Node root1 = a.createNode(2);
+		root1.left = a.createNode(7);
+		root1.right = a.createNode(8);
+		root1.left.left = a.createNode(5);
+
+		Node root2 = a.createNode(3);
+		root2.left = a.createNode(6);
+		root2.right = a.createNode(7);
+		root2.right.right = a.createNode(5);
+
+//		System.out.println("chek if both are mirrors :");
+//		System.out.println(a.checkMirror(root1, root2));
+//		System.out.println();
 		
-		System.out.println("Leve of a binar tree :");
-		System.out.println(a.getLevelOfNode(root, 7, 1));
+		System.out.println("chek if both are mirrors structures :");
+		System.out.println(a.checkMirrorStruct(root1, root2));
 		System.out.println();
 	}
 
